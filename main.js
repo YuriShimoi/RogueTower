0c04863234e3aee699c6aad18eb9df2940d51243
Yuri Shimoi <shimoi.yuri@gmail.com>
2023-02-08 01:06:08 -0300

    raids with fixed enemies and random path

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var MAPPING = [];
+var MAPPING = null;
+var CONTROL = null;
 
 function startGame() {
     document.getElementById("startMenu").setAttribute("hidden", true);
 
     setTimeout(() => {
         MAPPING = new TableMap(29, 15, document.getElementById("content"));
+        CONTROL = new RaidControl(MAPPING);
         MAPPING.plot();
     }, 200);
 }
